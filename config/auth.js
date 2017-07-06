66d0de511244dbf270f996c05e98b8f18c09cbc8
dandersonstack <danieltanderson@berkeley.edu>
2017-07-06 15:12:46 -0700

    moved callback to env var

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
   facebookAuth: {
     clientID: process.env.FB_CLIENT_ID,
     clientSecret: process.env.FB_CLIENT_SECRET,
-    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    callbackURL: `${process.env.HOST}/auth/facebook/callback`,
   }
 };
 // module.exports = {
